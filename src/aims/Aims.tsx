32a3674f61d67ff12b2544e42e3526fa9e992c0d
Jon Hall <code@jall.me>
2020-07-21 23:22:19 +0100

    Add disabled state to effort remover

--- src/aims/Aims.tsx
+++ src/aims/Aims.tsx
@@ -28,82 +28,86 @@ interface Props {
 export default function Aims({aims, upsert, remove}: Props) {
   return (
     <List spacing={3}>
-      {aims.map((aim) => (
-        <ListItem key={aim.id}>
-          <Stack
-            isInline={true}
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Stack isInline={true} alignItems="center">
-              <ListIcon icon="chevron-right" />
-              <Tag>
-                {aim.efforts.reduce(
-                  (total, effort) => total + effort.amount,
-                  0,
-                )}
-              </Tag>
-              <Editable
-                defaultValue={aim.title}
-                onChange={(title) => upsert({...aim, title})}
-              >
-                <EditablePreview />
-                <EditableInput />
-              </Editable>
+      {aims.map((aim) => {
+        const totalEfforts = aim.efforts.reduce(
+          (total, effort) => total + effort.amount,
+          0,
+        )
+        return (
+          <ListItem key={aim.id}>
+            <Stack
+              isInline={true}
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Stack isInline={true} alignItems="center">
+                <ListIcon icon="chevron-right" />
+                <Tag>{totalEfforts}</Tag>
+                <Editable
+                  defaultValue={aim.title}
+                  onChange={(title) => upsert({...aim, title})}
+                >
+                  <EditablePreview />
+                  <EditableInput />
+                </Editable>
+              </Stack>
+
+              <Stack isInline={true} alignItems="center">
+                <IconButton
+                  aria-label="Increment effort"
+                  icon="add"
+                  onClick={() =>
+                    upsert({
+                      ...aim,
+                      // TODO Buffer inputs to collapse rapid clicks into a single Effort
+                      // Or find a prebuilt component that does it for me
+                      efforts: [
+                        ...aim.efforts,
+                        {id: uuid.v4(), amount: 1, achievedAt: new Date()},
+                      ],
+                    })
+                  }
+                  bg="green.400"
+                  color="white"
+                />
+
+                <IconButton
+                  aria-label="Remove most recent effort"
+                  onClick={() => {
+                    const [first, ...rest] = aim.efforts
+                    const mostRecentEffort = rest.reduce(
+                      (mostRecent, effort) =>
+                        effort.achievedAt > mostRecent.achievedAt
+                          ? effort
+                          : mostRecent,
+                      first,
+                    )
+                    upsert({
+                      ...aim,
+                      efforts: aim.efforts.filter(
+                        (e) => e.id !== mostRecentEffort.id,
+                      ),
+                    })
+                  }}
+                  icon="minus"
+                  bg="red.500"
+                  color="white"
+                  isDisabled={totalEfforts === 0}
+                />
+
+                <Menu>
+                  <MenuButton as={Button}>...</MenuButton>
+                  <MenuList>
+                    <MenuItem onClick={() => remove(aim.id)}>
+                      Delete aim
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </Stack>
             </Stack>
-
-            <Stack isInline={true} alignItems="center">
-              <IconButton
-                aria-label="Increment effort"
-                icon="add"
-                onClick={() =>
-                  upsert({
-                    ...aim,
-                    // TODO Buffer inputs to collapse rapid clicks into a single Effort
-                    // Or find a prebuilt component that does it for me
-                    efforts: [
-                      ...aim.efforts,
-                      {id: uuid.v4(), amount: 1, achievedAt: new Date()},
-                    ],
-                  })
-                }
-                bg="green.400"
-                color="white"
-              />
-
-              <IconButton
-                aria-label="Remove most recent effort"
-                onClick={() => {
-                  const [first, ...rest] = aim.efforts
-                  const mostRecentEffort = rest.reduce(
-                    (mostRecent, effort) =>
-                      effort.achievedAt > mostRecent.achievedAt
-                        ? effort
-                        : mostRecent,
-                    first,
-                  )
-                  upsert({
-                    ...aim,
-                    efforts: aim.efforts.filter(
-                      (e) => e.id !== mostRecentEffort.id,
-                    ),
-                  })
-                }}
-                icon="minus"
-                bg="red.500"
-                color="white"
-              />
-
-              <Menu>
-                <MenuButton as={Button}>...</MenuButton>
-                <MenuList>
-                  <MenuItem onClick={() => remove(aim.id)}>Delete aim</MenuItem>
-                </MenuList>
-              </Menu>
-            </Stack>
-          </Stack>
-        </ListItem>
-      ))}
+          </ListItem>
+        )
+      })}
 
       <AddNew add={upsert} />
     </List>
