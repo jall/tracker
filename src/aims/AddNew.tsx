4ea765a88488380371a3b694da44ddd373540985
Jon Hall <code@jall.me>
2020-07-21 23:19:57 +0100

    Add aim on enter
    As well as pressing the button

--- src/aims/AddNew.tsx
+++ src/aims/AddNew.tsx
@@ -16,35 +16,41 @@ interface Props {
 export default function AddNew({add}: Props) {
   const [title, setTitle] = useState<string | null>(null)
   return (
-    <Stack isInline>
-      <FormControl>
-        <Input
-          type="text"
-          id="title"
-          aria-describedby="title-helper-text"
-          value={title || ""}
-          onChange={(event: FormEvent<HTMLInputElement>) =>
-            setTitle(event.currentTarget.value)
-          }
-        />
-        <FormHelperText id="title-helper-text">Add a new aim</FormHelperText>
-      </FormControl>
+    <form
+      action="#"
+      onSubmit={(event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
 
-      <Button
-        aria-label="Add aim"
-        leftIcon="add"
-        type="submit"
-        bg="green.400"
-        color="white"
-        onClick={() => {
-          if (title) {
-            add({id: uuid.v4(), title})
-          }
-          setTitle(null)
-        }}
-      >
-        Add
-      </Button>
-    </Stack>
+        if (title) {
+          add({id: uuid.v4(), title})
+        }
+
+        setTitle(null)
+      }}
+    >
+      <Stack isInline>
+        <FormControl>
+          <Input
+            type="text"
+            id="title"
+            aria-describedby="title-helper-text"
+            value={title || ""}
+            onChange={(event: FormEvent<HTMLInputElement>) =>
+              setTitle(event.currentTarget.value)
+            }
+          />
+          <FormHelperText id="title-helper-text">Add a new aim</FormHelperText>
+        </FormControl>
+        <Button
+          aria-label="Add aim"
+          leftIcon="add"
+          type="submit"
+          bg="green.400"
+          color="white"
+        >
+          Add
+        </Button>
+      </Stack>
+    </form>
   )
 }
